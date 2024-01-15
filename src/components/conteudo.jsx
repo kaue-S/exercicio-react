5e6ea2d23a2c835ff6e569aebbeca9aab28b5e2c
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-01-15 09:14:48 -0300

    comit

--- src/components/conteudo.jsx
+++ src/components/conteudo.jsx
@@ -1,19 +1,18 @@
 import styled from "styled-components";
 
+
 const StyledConteudo = styled.menu`
-    border: 10px solid black;
-    border-radius: 0 10px 10px 0;
-    width: 70%;
-    margin: 0;
+  border: 10px solid black;
+  border-radius: 0 10px 10px 0;
+  width: 70%;
+  margin: 0;
 `;
 
-
-function Conteudo(){
-    return <StyledConteudo>
+function Conteudo() {
+  return (
+    <StyledConteudo>
     </StyledConteudo>
-    
-       
-   
+  );
 }
 
 export default Conteudo;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 function Home(){
     return <a href="">Home</a>;
+    
 }
 
 export default Home;
